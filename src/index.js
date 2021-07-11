a4f5832f18dc20aa4b7410d6cafc69e8a07a5441
danainjax <danainjax@gmail.com>
2021-07-11 10:25:01 -0400

    added font to form and stored textarea in a global variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const posts = document.getElementById("posts-container")
 const newPost = document.getElementById("new-post")
 const categories = document.getElementById("categories-container")
+const textarea = document.getElementById("text")
 const baseURL = "http://localhost:3000"
 const postService = new PostService();
 const categoryService = new CategoryService();
