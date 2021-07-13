6be7a28dbfc62dcbd135b3a889bb912804bc2d78
danainjax <danainjax@gmail.com>
2021-07-13 13:00:59 -0400

    removed extra console.log

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -8,7 +8,6 @@ class CategoryService {
         this.getCategories().then((categories) => {
             categories.forEach((category) => {
                 let c = new Category(category);
-                // console.log(category)
             })
         })
     }
